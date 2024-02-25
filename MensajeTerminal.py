69ca87de3e542fba2293916d5e6b6fcf6beafe99
ma0oavil93 <158866412+ma0oavil93@users.noreply.github.com>
2024-02-25 16:05:50 -0700

    Add files via upload

---
+++ MensajeTerminal.py
@@ -0,0 +1,10 @@
+def main():
+    # Solicitar al usuario que ingrese un mensaje
+    mensaje = input("Por favor, ingresa un mensaje: ")
+
+    # Mostrar el mensaje en pantalla
+    print("El mensaje ingresado es:", mensaje)
+
+if __name__ == "__main__":
+    main()
+
